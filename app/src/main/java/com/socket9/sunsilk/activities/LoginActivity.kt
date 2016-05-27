a4d630a7ea750ae20cf3ff478a7704a12d92e74b
phuchit <ripzery@gmail.com>
2016-05-27 11:28:37 +0700

    Initial commit

---
+++ app/src/main/java/com/socket9/sunsilk/activities/LoginActivity.kt
@@ -0,0 +1,25 @@
+package com.socket9.sunsilk.activities
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.socket9.sunsilk.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        initInstance()
+        initListener()
+
+    }
+
+    private fun initListener() {
+
+    }
+
+    private fun initInstance() {
+
+    }
+}
